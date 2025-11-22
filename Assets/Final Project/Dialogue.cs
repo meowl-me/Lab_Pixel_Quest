20666a94976dfccc9c2b0bb5103b95cd244e273e
dalesgames167 <cwriterfantasypeaced@gmail.com>
2025-11-22 11:56:40 -0500

    11/22/25

--- Assets/Final Project/Dialogue.cs
+++ Assets/Final Project/Dialogue.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HW3PlayerDialogue : MonoBehaviour
+public class Dialogue : MonoBehaviour
 {
     public List<string> dialogue = new List<string>();
 
